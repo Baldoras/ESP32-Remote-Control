4564b757d1e71639cdeb189c6cd533cf25886ada
Baldoras <stefan.sebastian.schramm@gmail.com>
2025-12-11 17:15:48 +0100

    Add file for global objects
    ADD: globals.h for global object declaration

---
+++ Globals.h
@@ -0,0 +1,31 @@
+/*
+*
+* Global object definition
+*
+*/
+
+#pragma once
+
+#include "DisplayHandler.h"
+#include "UIPageManager.h"
+#include "BatteryMonitor.h"
+#include "JoystickHandler.h"
+#include "SDCardHandler.h"
+#include "TouchManager.h"
+#include "config.h"
+
+extern DisplayHandler display;
+extern UIPageManager pageManager;
+extern BatteryMonitor battery;
+extern JoystickHandler joystick;
+extern SDCardHandler sdCard;
+extern TouchManager touch;
+
+// Seiten-IDs
+enum PageID {
+    PAGE_HOME = 0,
+    PAGE_REMOTE = 1,
+    PAGE_CONNECTION = 2,
+    PAGE_SETTINGS = 3,
+    PAGE_INFO = 4
+};
